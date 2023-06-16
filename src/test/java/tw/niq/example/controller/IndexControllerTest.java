c1f6ae5ab37f0a39806b4e89cd9a500d080d6f3b
Nick <nick.chen@cmp.com.tw>
2023-06-16 16:06:27 +0800

    Add index test

---
+++ src/test/java/tw/niq/example/controller/IndexControllerTest.java
@@ -0,0 +1,55 @@
+package tw.niq.example.controller;
+
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.httpBasic;
+import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+@WebMvcTest
+class IndexControllerTest {
+	
+	@Autowired
+	WebApplicationContext webApplicationContext;
+	
+	MockMvc mockMvc;
+
+	@BeforeEach
+	void setUp() throws Exception {
+		mockMvc = MockMvcBuilders
+				.webAppContextSetup(webApplicationContext)
+				.apply(springSecurity())
+				.build();
+	}
+
+	@WithMockUser("admin")
+	@Test
+	void testIndex_withAdminUser() throws Exception {
+		mockMvc.perform(get("/"))
+			.andExpect(status().isOk())
+			.andExpect(view().name("index"));
+	}
+	
+	@Test
+	void testIndex_withAdminCredentials() throws Exception {
+		mockMvc.perform(get("/").with(httpBasic("admin", "admin")))
+			.andExpect(status().isOk())
+			.andExpect(view().name("index"));
+	}
+	
+	@Test
+	void testIndex_withBadAdminCredentials() throws Exception {
+		mockMvc.perform(get("/").with(httpBasic("admin", "password")))
+			.andExpect(status().isUnauthorized());
+	}
+
+}
